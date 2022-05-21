0c38d7822a235315a3dfcaab546aacb3a4b27956
Matt Nickodemus <matthewnickodemus@gmail.com>
2022-05-21 17:53:17 -0600

    changed documentation for set_edify_password

--- R/set_edify_password.R
+++ R/set_edify_password.R
@@ -1,9 +1,19 @@
-#' Reset Edify Password
+#' Set Edify password
 #'
-#' @return
-#' @export
+#' `set_edify_password` will prompt you with a dialog box to input your new password
+#' from edify.
 #'
-#' @examples
+#' @details **To reset your Edify password**
+#'
+#' @details
+#'
+#' - Navigate to \href{https://dixie.edh.eab.com/}{Edify}.
+#' - Select the SSO login.
+#' - Login with your UT single sign-on credentials.
+#' - Click the question mark in the upper left corner.
+#' - Select User Profile
+#' - Select Generate new credential.
+#'
 set_edify_password <- function() {
   keyring::key_set("edify", "password")
 }
