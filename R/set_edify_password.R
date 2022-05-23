d5d792f36218014f2fd76fadf7a3a23126bcd470
Matt Nickodemus <matthewnickodemus@gmail.com>
2022-05-23 12:31:32 -0600

    added keyring to import for set_edify_password

--- R/set_edify_password.R
+++ R/set_edify_password.R
@@ -14,6 +14,8 @@
 #' - Select User Profile
 #' - Select Generate new credential.
 #'
+#' @importFrom keyring key_set
+
 set_edify_password <- function() {
   keyring::key_set("edify", "password")
 }
