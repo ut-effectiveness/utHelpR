fedb968776406ed408acae3465ff49fd2a3f1c89
Matt Nickodemus <matthewnickodemus@gmail.com>
2022-05-23 18:57:29 -0600

    added two new functions display_sql_no_comment and display_sql_with_comment

--- R/markdown_helpers.R
+++ R/markdown_helpers.R
@@ -6,14 +6,31 @@
 #'
 #' @param file The name of the sql file you want to display.
 #'
-#' @return
 #' @export
 #'
+#' @importFrom readr read_lines
+#' @importFrom stringr str_detect
 
 display_sql_no_comments <- function(file) {
+
   x <- read_lines(here::here('sql', file))
 
   comments_removed <- x[!str_detect(x, '--')]
 
   paste0(comments_removed, collapse = '\n')
+}
+
+#' Display sql with comments
+#'
+#' @param file The name of the sql file you want to display.
+#'
+#' @export
+#'
+#'@importFrom readr read_file
+
+diplay_sql_with_comments <- function(file) {
+
+  x <- read_file(here::here('sql', 'clo_pull.sql'))
+
+  return(x)
 }
