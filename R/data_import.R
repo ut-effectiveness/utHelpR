1fba780f49e0f1ce0b5bf9b13d24cc850941f1b3
Brenten L. Canfield <brentenlavoy@gmail.com>
2022-06-03 16:21:04 -0600

    Added critical data import functions.

--- R/data_import.R
+++ R/data_import.R
@@ -69,9 +69,51 @@ get_data_from_sql_url <- function(query_url, dsn) {
 #' @export
 #'
 #' @examples
-
+#'
 load_data_from_rds <- function(file_name) {
   df <- readRDS( here::here('data', file_name) ) %>%
     mung_dataframe()
+  return(df)
+}
+
+#' Get data from a pin stored in RStudio Connect
+#'
+#' @param pin_name The name of the pin, as stored on RStudio Connect instance.
+#'
+#' @return a dataframe containing all data from requested pin
+#' @export
+#'
+#' @examples
+#' get_data_from_pin(pin_name="student_type_audit_student_type_determination_variables_pin")
+#'
+get_data_from_pin <- function(pin_name) {
+  # Obtain the API key from environment variable.
+  api_key <- Sys.getenv("RSCONNECT_SERVICE_USER_API_KEY")
+  # If API key is not available as environment variable, use keyring entry.
+  # NOTE: The API key should only be an environment variable on the server
+  #       For local machines, set a keyring entry.
+  if (api_key == "") {
+    api_key <- keyring::key_get("pins", "api_key")
+  }
+  # Register the connection to the pinning board.
+  board_register_rsconnect(key=api_key, server="https://data.dixie.edu")
+  # pull data from the pin
+  df <- pin_get(pin_name, board="rsconnect") %>%
+    mung_dataframe()
+  return(df)
+}
+
+#' Load data from an Excel spreadsheet formatted file
+#'
+#' @param file_name the name of the file, assumed to be located in a directory called 'data'
+#'
+#' @return a dataframe with all content of the Excel spreadsheet
+#' @export
+#'
+#' @importFrom readxl read_excel
+#' @examples
+#' load_data_from_xlsx("additional_information.xlsx")
+load_data_from_xlsx <- function(file_name) {
+  df <- read_excel( here::here('data', file_name) )
   return(df)
 }
