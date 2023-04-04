55c882d5f38f2cb82667292854eb5b774a18dc93
jpfeifer-dsu <justin.pfeifer@utahtech.edu>
2023-04-04 14:33:33 -0600

    fixed issue with board and change pin_get to pin_read per pins documentation

--- R/data_import.R
+++ R/data_import.R
@@ -80,9 +80,9 @@ get_data_from_pin <- function(pin_name) {
     api_key <- keyring::key_get("pins", "api_key")
   }
   # Register the connection to the pinning board.
-  pins::board_register_rsconnect(key=api_key, server="https://rs-connect.utahtech.edu/")
+  rsconnect <- pins::board_register_rsconnect(key=api_key, server="https://rs-connect.utahtech.edu/")
   # pull data from the pin
-  df <- pins::pin_get(pin_name, board="rsconnect") %>%
+  df <- pins::pin_read(pin_name, board=rsconnect) %>%
     mung_dataframe()
   return(df)
 }
