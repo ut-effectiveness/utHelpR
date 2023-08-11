525e041d95c27dc5fd2ce2d811e1591a92c5ed37
cdemkegit <craig.demke@utahtech.edu>
2023-08-11 14:22:18 -0600

    Added some updates to the make_standard_folders function. I also wanted to test to see is we could have a Rmarkdown template hosted in the package itself.

--- R/project_util.R
+++ R/project_util.R
@@ -8,15 +8,34 @@
 #' @export
 #'
 #' @importFrom purrr map
-#' @importFrom fs dir_ls
+#' @importFrom fs dir_ls file_create
+#' @importFrom usethis use_git use_blank_slate
+#' @importFrom here here
 
 make_standard_folders <- function() {
+  usethis::use_blank_slate(scope = c("user", "project"))
   # Test to see if there is a .Rproj file in the present working directory.
   if( length(fs::dir_ls(".", glob = '*.Rproj')) != 1 ) {
-    stop('There is no .Rproj file in the current directory. You should create a project in Rstudio before you create your directory structure. If you have already created a project, run getwd() in the console and check that your .Rproj file is in that directory. If you have further questions Slack Matt, Justin, or LaVoy.')
+    stop('There is no .Rproj file in the current directory. You should create a project in Rstudio before you create your directory structure. If you have already created a project, run getwd() in the console and check that your .Rproj file is in that directory. If you have further questions Slack Matt, or Craig.')
   }
 
   folders <- c('data', 'sensitive', 'markdown', 'R', 'sql')
 
-  pitch <- map(folders, fs::dir_create)
+  pitch <- purrr::map(folders, fs::dir_create)
+
+  if (file.exists(here::here(".gitignore")))
+  {
+    fs::file_create("sensitive/test")
+    cat("this is a test", file = here::here("sensitive/test"), append = TRUE)
+    cat("sensitive/", file = here::here(".gitignore"), append = TRUE, sep = "\n")
+  }
+  else {
+    usethis::use_git()
+    fs::file_create("sensitive/test")
+    cat("Make sure that this file is not showing up in your git tab. ",
+      file = here::here("sensitive/test"), append = TRUE)
+    cat("sensitive/", file = here::here(".gitignore"), append = TRUE, sep = "\n")
+  }
 }
+
+
