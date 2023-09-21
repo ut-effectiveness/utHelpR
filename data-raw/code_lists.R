7dd14e902aa80850e7ee642d88bd906d238436c5
Matt Nickodemus <matthewnickodemus@gmail.com>
2023-09-21 10:47:50 -0500

    added code to generate the data frame fake_enrollment. Also aded the data frame to the package.

---
+++ data-raw/code_lists.R
@@ -0,0 +1,10 @@
+gender_code_list <- c(rep('M', 100), rep('F', 100), 'N', rep(NA, 25))
+
+residential_housing_code_list <- c('N','R','C','H','0','M','G','S','A')
+
+student_type_code_list <- c('N','2','R','C','T','3','P','H','0','5','1','F','S')
+
+primary_level_class_id_list <- c('JR','SR','FR','GG','SO', NA)
+
+primary_degree_id_list <- c('BS','BM','BAS','MAT','BME','CER1','AA','AB','MACC','AC',
+                            'BIS','BA','TR','MA','AS','ND','CER0','BSN','AAS','BFA','MMFT','APE', NA)
