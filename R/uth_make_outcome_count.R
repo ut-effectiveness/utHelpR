f281ad4fbe0fc36b7e621522d5f6c7b1d3097e96
Matt Nickodemus <matthewnickodemus@gmail.com>
2023-09-21 11:09:47 -0500

    put some polish on the description of uth_make_outcome_count.

--- R/uth_make_outcome_count.R
+++ R/uth_make_outcome_count.R
@@ -1,9 +1,23 @@
-#' uth_make_outcome_count
+#' Make a table of column aggregation counts
 #'
-#' @param .data A data frame with columns you would like to aggregate on
-#' @param ... the columns you would lke to aggregate over
+#' @description
 #'
-#' @return
+#' Use this function to aggregate counts in a data frame. A typical use case
+#' would involve a data frame with student ids and a collection of attributes. The utHelpR
+#' package includes a data frame called fake_enrollment with this structure. The following
+#' code illustrates how to use uth_make_outcome_count
+#'
+#' test <- fake_enrollment %>%
+#'  select(student_id, gender_code) %>%
+#'  filter(!is.na(gender_code))
+#'
+#'  uth_make_outcome_count(test, gender_code)
+#'
+#' @param .data A data frame with columns you would like to aggregate on.
+#' @param ... The columns you would like to aggregate over. Don't forget the
+#' column you are counting, i.e. student number.
+#'
+#' @return A table with aggregated counts of column attributes
 #' @export
 #'
 #' @import dplyr
